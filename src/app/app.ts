a5e64b62f8140d692646aaba0146882334c1735c
Cloud9-web <tdlkaviya2020@gmail.com>
2026-02-13 10:40:06 +0530

    Component Update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from "./menu/menu";
+import { SubMenu } from "./menu/sub-menu/sub-menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu, SubMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('MyApp');
-}
+}                                                                                                 
---
+++ src/app/menu/sub-menu/sub-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-menu',
+  imports: [],
+  templateUrl: './sub-menu.html',
+  styleUrl: './sub-menu.css',
+})
+export class SubMenu {
+
+}
